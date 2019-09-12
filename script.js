05cdc0b2e65e9f6afae3508f2c5dd98703a342c6
Alexey <54054357+alexerisov@users.noreply.github.com>
2019-09-12 18:26:07 +0700

    Add level function

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+function level (plan) {
+
+this.width = 10;
+this.height = 10;
+this.grid = new Array[width * height];
+
+for (let i = 0; i < height; i++) {
+ for (let j = 0; j < width; j++) {
+
+} 
+} 
+
+} 
+
