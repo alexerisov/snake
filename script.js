6ce6193e14c701ca3670fcb37f7119cbb21f2d8b
alexerisov <alexerisov@gmail.com>
2019-09-13 01:41:09 +0700

    scripts moved into script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 'use strict';
 
+
+    let gameDiv = document.getElementById("game");
+    let canvas = document.createElement("canvas");
+    canvas.setAttribute("width", "500");
+    canvas.setAttribute("height", "500");
+    gameDiv.appendChild(canvas);
+
+    let canvas = document.querySelector("canvas")
+    let cx = canvas.getContext("2d");
+    cx.strokeStyle = "black";
+    cx.lineWidth = 1;
+    
+    let number = 25,
+        step = 20;
+
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+        cx.strokeRect(i*step, j*step, step, step);
+        }
+    }
+
+
+
 function level (plan) {
 
 this.width = 10;
