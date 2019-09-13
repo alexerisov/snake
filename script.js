8562e8104506022bbcc72b4f6a30475d9af8e4f8
alexerisov <alexerisov@gmail.com>
2019-09-14 03:04:35 +0700

    restyling, add dat.gui, refactoring html

--- script.js
+++ script.js
@@ -1,35 +1,170 @@
-'use strict';
+function drawLevel (width, heigth) {
+  const space = new Array(heigth);
+  this.width = width;
+  this.heigth = heigth;
 
-function level (plan) {
-  this.width = 10;
-  this.height = 10;
-  this.grid = new Array[width * height]();
+  for (let y = 0; y < heigth; y++) {
+    let line = '';
+    for (let x = 0; x < width; x++) {
+      switch (true) {
+        case (x === 0 || x === width - 1):
+          line += '#';
+          break;
+        case (y === 0 || y === heigth - 1):
+          line += '#';
+          break;
+        case (x === 12 && y === 12):
+          line += 's';
+          break;
 
-  for (let i = 0; i < this.height; i++) {
-    for (let j = 0; j < this.width; j++) {
-
+        default:
+          line += ' ';
+          break;
+      }
+      space[y] = line;
     }
+  }
+  return space;
+}
+
+const level = drawLevel(25, 25);
+console.log(level);
+
+function Vector (x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function (other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+function Grid (width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+Grid.prototype.isInside = function (vector) {
+  return vector.x >= 0 && vector.x < this.width &&
+    vector.y >= 0 && vector.y < this.height;
+};
+Grid.prototype.get = function (vector) {
+  return this.space[vector.x + this.width * vector.y];
+};
+Grid.prototype.set = function (vector, value) {
+  this.space[vector.x + this.width * vector.y] = value;
+};
+var directions = {
+  n: new Vector(0, -1),
+  ne: new Vector(1, -1),
+  e: new Vector(1, 0),
+  se: new Vector(1, 1),
+  s: new Vector(0, 1),
+  sw: new Vector(-1, 1),
+  w: new Vector(-1, 0),
+  nw: new Vector(-1, -1)
+};
+
+var guiObj = {
+  snakeColor: '#0b401b',
+  strokeColor: '#181717'
+};
+
+function elementFromChar (legend, ch) {
+  if (ch === ' ') {
+    return null;
+  }
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+function World (map, legend) {
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+
+  map.forEach(function (line, y) {
+    for (var x = 0; x < line.length; x++) {
+      grid.set(new Vector(x, y),
+        elementFromChar(legend, line[x]));
+    }
+  });
+}
+
+function charFromElement (element) {
+  if (element == null) {
+    return ' ';
+  } else {
+    return element.originChar;
   }
 }
 
+var world = new World(level, {
+  '#': Wall,
+  's': Snake,
+  '@': Apple
+});
+
 function displayCanvas () {
-  const gameDiv = document.getElementById('game');
-  let canvas = document.createElement('canvas');
+  const cellSize = 20;
+  var canvas = document.querySelector('canvas');
   canvas.setAttribute('width', '500');
   canvas.setAttribute('height', '500');
-  gameDiv.appendChild(canvas);
 
-  canvas = document.querySelector('canvas');
   const cx = canvas.getContext('2d');
-  cx.strokeStyle = 'black';
-  cx.lineWidth = 1;
+  cx.clearRect(0, 0, cx.width, cx.height);
 
-  const number = 25;
-        let step = 20;
+  function drawRect (x, y, size, color) {
+    cx.fillStyle = color;
+    cx.fillRect(x * size, y * size, size, size);
+  }
 
-  for (let i = 0; i < number; i++) {
-    for (let j = 0; j < number; j++) {
-      cx.strokeRect(i * step, j * step, step, step);
+  function drawStroke (x, y, size, color) {
+    cx.strokeStyle = color;
+    cx.lineWidth = 1;
+    cx.strokeRect(x * size, y * size, size, size);
+  }
+
+  function drawWall (x, y) {
+    const gradient = cx.createLinearGradient(x * cellSize, y * cellSize, (x + 1) * cellSize, (y + 1) * cellSize);
+    gradient.addColorStop(0, 'rgb(50, 50, 50)');
+    gradient.addColorStop(1, 'rgb(35, 35, 35)');
+    drawRect(x, y, cellSize, gradient);
+    // drawStroke(x, y, cellSize, 'rgb(45, 45, 45)');
+  }
+
+  function drawSnake (x, y) {
+    drawRect(x, y, cellSize, guiObj.snakeColor);
+  }
+
+  function drawApple (x, y) {
+    drawRect(x, y, cellSize, 'red');
+  }
+
+  function drawEmptySpace (x, y) {
+    drawRect(x, y, cellSize, '#222222');
+    drawStroke(x, y, cellSize, guiObj.strokeColor);
+  }
+
+  for (let y = 0; y < level.length; y++) {
+    const line = level[y];
+    for (let x = 0; x < level[0].length; x++) {
+      const element = line[x];
+
+      switch (element) {
+        case '#':
+          drawWall(x, y);
+          break;
+        case 's':
+          drawSnake(x, y);
+          break;
+        case '@':
+          drawApple(x, y);
+          break;
+        default:
+          drawEmptySpace(x, y);
+          break;
+      }
     }
   }
 }
